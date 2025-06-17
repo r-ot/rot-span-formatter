4193d07751f0f1b89e9729c6ddbb25c2bd973918
r-ot <reinhard@r-ot.at>
2025-06-17 17:05:19 +0200

    Initial commit

---
+++ block-editor.js
@@ -0,0 +1,47 @@
+(function () {
+  const { registerFormatType, applyFormat, removeFormat } = wp.richText;
+  const { RichTextToolbarButton } = wp.blockEditor;
+  const { Fragment } = wp.element;
+  const { __ } = wp.i18n;
+
+  registerFormatType('rot/span-class', {
+    title: __('Span mit Klasse', 'rot'),
+    tagName: 'span',
+    className: 'rot-span',
+    attributes: {
+      class: 'class',
+    },
+    edit({ isActive, value, onChange, activeAttributes }) {
+      const currentClass = activeAttributes.class || '';
+
+      const onToggle = () => {
+        if (isActive) {
+          onChange(removeFormat(value, 'rot/span-class'));
+        } else {
+          const enteredClass = window.prompt(__('CSS-Klasse für <span>', 'rot'), currentClass || 'highlight');
+          if (enteredClass) {
+            onChange(
+              applyFormat(value, {
+                type: 'rot/span-class',
+                attributes: {
+                  class: enteredClass,
+                },
+              })
+            );
+          }
+        }
+      };
+
+      return wp.element.createElement(
+        Fragment,
+        null,
+        wp.element.createElement(RichTextToolbarButton, {
+          icon: 'editor-code',
+          title: __('Span mit Klasse', 'rot'),
+          onClick: onToggle,
+          isActive: isActive,
+        })
+      );
+    },
+  });
+})();
---
+++ rot-span-formatter.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Rot Span Formatter
+ * Description: Fügt dem Gutenberg-Editor eine Möglichkeit hinzu, Text in <span> mit eigener CSS-Klasse zu wrappen.
+ * Version: 1.0
+ * Author: Du, Bro 😎
+ */
+
+add_action('enqueue_block_editor_assets', function () {
+    wp_enqueue_script(
+        'rot-span-formatter',
+        plugin_dir_url(__FILE__) . 'block-editor.js',
+        [ 'wp-rich-text', 'wp-element', 'wp-editor', 'wp-components', 'wp-i18n' ],
+        filemtime(plugin_dir_path(__FILE__) . 'block-editor.js'),
+        true
+    );
+});
+
+
+require_once plugin_dir_path(__FILE__) . 'github-updater.php';
